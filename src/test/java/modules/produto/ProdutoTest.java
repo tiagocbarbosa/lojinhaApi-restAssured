259bb5dd2b8ed29ac0fc8a21ef8d83e99dd073cf
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2022-02-08 07:53:02 -0300

    Dependencies added and first Test Class created

---
+++ src/test/java/modules/produto/ProdutoTest.java
@@ -0,0 +1,15 @@
+package modules.produto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes de API REST do módulo de Produto")
+public class ProdutoTest {
+
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do Produto")
+    public void testValidarLimitesProibidosValorProduto() {
+
+    }
+}
