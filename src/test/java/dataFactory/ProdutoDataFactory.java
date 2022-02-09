7d2c3ea2273245ea77565b00aea34eb87a31bdba
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2022-02-09 13:57:02 -0300

    New method created

--- src/test/java/dataFactory/ProdutoDataFactory.java
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -8,11 +8,15 @@ import java.util.List;
 
 public class ProdutoDataFactory {
 
-    public static ProdutoPojo criarProdutoBasicoComValorIgualA(double valor) {
+    private static List<String> criarCoresBrancoEPreto() {
         List<String> cores = new ArrayList<>();
         cores.add("Branco");
         cores.add("Preto");
 
+        return cores;
+    }
+
+    private static List<ComponentePojo> criarComponentesBasicos() {
         ComponentePojo componente = new ComponentePojo();
         componente.setComponenteNome("Controle");
         componente.setComponenteQuantidade(2);
@@ -20,12 +24,26 @@ public class ProdutoDataFactory {
         List<ComponentePojo> componentes = new ArrayList<>();
         componentes.add(componente);
 
+        return componentes;
+    }
+
+    public static ProdutoPojo criarProdutoBasicoComValorIgualA(double valor) {
         ProdutoPojo produto = new ProdutoPojo();
         produto.setProdutoNome("Playstation 5");
-        produto.setProdutoValor(0.00);
-        produto.setProdutoCores(cores);
+        produto.setProdutoValor(valor);
+        produto.setProdutoCores(criarCoresBrancoEPreto());
         produto.setProdutoUrlMock("");
-        produto.setComponentes(componentes);
+        produto.setComponentes(criarComponentesBasicos());
+
+        return produto;
+    }
+
+    public static ProdutoPojo criarProdutoSemCampoProdutoValor() {
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("Playstation 5");
+        produto.setProdutoCores(criarCoresBrancoEPreto());
+        produto.setProdutoUrlMock("");
+        produto.setComponentes(criarComponentesBasicos());
 
         return produto;
     }
